130274f4c63056faba9891d19b35edad890067e5
Jingang Miao <jmiao@google.com>
2019-12-18 14:52:34 -0500

    Drop python 2 support.
    PiperOrigin-RevId: 286236371

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
         'Topic :: Scientific/Engineering :: Mathematics',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
